7aedaf047af7f0f14f9276e2844dcbd8cc3822eb
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 20:18:33 +0600

    Add project files.

---
+++ Praktika_2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Praktika_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите два числа");
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = a + b;
+            Console.WriteLine("Сумма чисел " + a + " и " + b + " равна " + c);
+        }
+    }
+}
